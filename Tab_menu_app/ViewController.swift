393c5fc49ac9ea9b1ee83b24557a7ee24ca78e59
nitaking <johin@example.com>
2016-04-24 15:52:17 +0900

    ファイル追加　

--- Tab_menu_app/ViewController.swift
+++ Tab_menu_app/ViewController.swift
@@ -8,16 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//@UIApplicationMain
+class ViewController: UIViewController, UIAccelerometerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
